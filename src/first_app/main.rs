5ec3a83dd5680cd69f9811f0983fa41cd9f5e5f8
major <majorx234@googlemail.com>
2022-04-29 02:01:05 +0200

    Add first gtk application

---
+++ src/first_app/main.rs
@@ -0,0 +1,12 @@
+use gtk::prelude::*;
+use gtk::Application;
+
+fn main() {
+    // Create a new Application;
+    let app = Application::builder()
+        .application_id("org.gtk-rs.example")
+        .build();
+
+    // Run the application
+    app.run();
+}
