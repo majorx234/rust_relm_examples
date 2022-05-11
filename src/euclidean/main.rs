08e153877626435e3ec099ef8aba1d1027b3ab5e
major <majorx234@googlemail.com>
2022-05-11 23:53:36 +0200

    Add structure for GUI Application for euclidean algorithm

--- src/euclidean/main.rs
+++ src/euclidean/main.rs
@@ -2,7 +2,12 @@ use gtk::prelude::{BoxExt, ButtonExt, GtkWindowExt, OrientableExt};
 use relm4::{gtk, send, AppUpdate, Model, RelmApp, Sender, WidgetPlus, Widgets};
 use std::cmp;
 
-fn greatest_common_deivsor(a_in: u32, b_in: u32) -> u32 {
+#[derive(Default)]
+struct AppModel {
+    greatest_devisor: u32,
+}
+
+fn greatest_common_devisor(a_in: u32, b_in: u32) -> u32 {
     let mut a = cmp::max(a_in, b_in);
     let mut b = cmp::min(a_in, b_in);
 
@@ -14,9 +19,63 @@ fn greatest_common_deivsor(a_in: u32, b_in: u32) -> u32 {
     return a;
 }
 
+enum AppMsg {
+    Calculate,
+}
+
+impl Model for AppModel {
+    type Msg = AppMsg;
+    type Widgets = AppWidgets;
+    type Components = ();
+}
+
+impl AppUpdate for AppModel {
+    fn update(&mut self, msg: AppMsg, _components: &(), _sender: Sender<AppMsg>) -> bool {
+        match msg {
+            AppMsg::Calculate => {
+                self.greatest_devisor = greatest_common_devisor(32, 48);
+            }
+        }
+        true
+    }
+}
+
+#[relm4::widget]
+impl Widgets<AppModel, ()> for AppWidgets {
+    view! {
+        gtk::ApplicationWindow {
+            set_title: Some("greatest common devisor"),
+            set_default_width: 300,
+            set_default_height: 100,
+            set_child = Some(&gtk::Box) {
+                set_orientation: gtk::Orientation::Vertical,
+                set_margin_all: 5,
+                set_spacing: 5,
+
+                append = &gtk::Button {
+                    set_label: "calculate",
+                    connect_clicked(sender) => move |_| {
+                        send!(sender, AppMsg::Calculate);
+                    },
+                },
+                append = &gtk::Label {
+                    set_margin_all: 5,
+                    set_label: watch! { &format!("Greatest Common Devisor: {}", model.greatest_devisor) },
+                }
+            },
+        }
+    }
+}
+
 fn main() {
-    let a: u32 = 16;
+    let model = AppModel::default();
+    let app = RelmApp::new(model);
+    app.run();
+}
+
+/*
+        let a: u32 = 16;
     let b: u32 = 24;
     let c: u32 = greatest_common_deivsor(a, b);
     println!("gcd of {} and {} is {}", a, b, c);
-}
+}*/
